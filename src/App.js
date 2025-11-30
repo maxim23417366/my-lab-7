8fec06ab4d93328e1ed29553037a30bb2c14f177
Maksym Neporozhnii <max.n.200206@gmail.com>
2025-11-30 17:50:01 +0200

    imgs2

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import GoodsCard from './components/GoodCard';
 
 // Оновлений масив з надійними посиланнями на картинки
 const goodsData = [
-  { id: 1, name: 'Creality Hyper PETG', price: 689, image: 'my-lab-7/public/images/Creality_Hyper_PETG.jpg'},
-  { id: 2, name: 'Creality TPU', price: 969, image: 'my-lab-7/public/images/Creality_TPU.jpg' },
-  { id: 3, name: 'Creality PETG', price: 645, image: 'my-lab-7/public/images/Creality_PETG.jpg' },
-  { id: 4, name: 'Creality PLA silky shine', price: 1165, image: 'my-lab-7/public/images/PLA_silky_shine.jpg' },
-  { id: 5, name: 'Creality PLA+', price: 794, image: 'my-lab-7/public/images/Creality_PLA+.jpg' },
-  { id: 6, name: 'Gembird ABS', price: 539, image: 'my-lab-7/public/images/Gembird_ABS.jpg' },
-  { id: 7, name: 'ELEGOO PETG', price: 499, image: 'my-lab-7/public/images/ELEGOO_PETG.jpg'}
+  { id: 1, name: 'Creality Hyper PETG', price: 689, image: 'images/Creality_Hyper_PETG.jpg'},
+  { id: 2, name: 'Creality TPU', price: 969, image: 'images/Creality_TPU.jpg' },
+  { id: 3, name: 'Creality PETG', price: 645, image: 'images/Creality_PETG.jpg' },
+  { id: 4, name: 'Creality PLA silky shine', price: 1165, image: 'images/PLA_silky_shine.jpg' },
+  { id: 5, name: 'Creality PLA+', price: 794, image: 'images/Creality_PLA+.jpg' },
+  { id: 6, name: 'Gembird ABS', price: 539, image: 'images/Gembird_ABS.jpg' },
+  { id: 7, name: 'ELEGOO PETG', price: 499, image: 'images/ELEGOO_PETG.jpg'}
 ];
 
 function App() {
